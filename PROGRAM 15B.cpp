45fc82df0f1708eacaeba028997a7b7562d98c6a
Yash Shrivastava <yashshrivastava578@gmail.com>
2025-09-17 10:38:57 +0530

    Create PROGRAM 15B.cpp

---
+++ PROGRAM 15B.cpp
@@ -0,0 +1,33 @@
+// NAME: YASH SHRIVASTAVA
+// PRN: 24070123137
+// BATCH: E&TC-B2
+// TITLE: ADDITION OF ALL INTEGERS UPTO N (EXPERIMENT: 15B)
+
+// CODE: 
+
+#include <iostream>
+using namespace std;
+
+int sumup(int n)
+{
+    if (n <= 0)
+        return 0;
+    else
+        return (n + sumup(n - 1));
+}
+
+int main()
+{
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    cout << "Sum of integers up to " << num << " is " << sumup(num);
+    return 0;
+}
+
+// OUTPUT:
+
+// Enter a number: 3
+// Sum of integers up to 3 is 6
+
